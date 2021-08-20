092f706165a7cd6518258709965745ee2838680a
Jatin Vishwakarma <kumarvishwakarma49@gmail.com>
2021-08-20 16:38:34 +0530

    day 21 coompleted

--- lib/Widget/themes.dart
+++ lib/Widget/themes.dart
@@ -27,7 +27,9 @@ class MyTheme {
           color: Colors.black,
           elevation: 0.0,
           iconTheme: IconThemeData(color: Colors.white),
-          textTheme: Theme.of(context).textTheme));
+          textTheme: Theme.of(context).textTheme.copyWith(
+              headline6:
+                  context.textTheme.headline6!.copyWith(color: Colors.white))));
 
   static Color darkColor = Vx.gray900;
   static Color lightBlue = Vx.indigo500;
